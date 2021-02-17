fb199f4ab8902810e97ca13d1f73ac8760bb3a27
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-02-16 19:41:03 -0500

    new gatsby portfolio

---
+++ .cache/async-requires.js
@@ -0,0 +1,14 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-template-archive-js": () => import("./../../../src/template/archive.js" /* webpackChunkName: "component---src-template-archive-js" */),
+  "component---src-template-blog-details-js": () => import("./../../../src/template/blog-details.js" /* webpackChunkName: "component---src-template-blog-details-js" */),
+  "component---src-template-category-post-js": () => import("./../../../src/template/category-post.js" /* webpackChunkName: "component---src-template-category-post-js" */),
+  "component---src-template-project-details-js": () => import("./../../../src/template/project-details.js" /* webpackChunkName: "component---src-template-project-details-js" */),
+  "component---src-template-tag-template-js": () => import("./../../../src/template/tag-template.js" /* webpackChunkName: "component---src-template-tag-template-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\pages\\index.js"))),
+  "component---src-template-archive-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\template\\archive.js"))),
+  "component---src-template-blog-details-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\template\\blog-details.js"))),
+  "component---src-template-category-post-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\template\\category-post.js"))),
+  "component---src-template-project-details-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\template\\project-details.js"))),
+  "component---src-template-tag-template-js": hot(preferDefault(require("C:\\Users\\maxpl\\OneDrive\\Documents\\Personal Projects\\Portfolio\\Portfolio\\src\\template\\tag-template.js")))
+}
+
