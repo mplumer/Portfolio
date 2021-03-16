a6cf7f5f16485eb34abbfc01f48a724f5439d2de
Max Plumer <68719068+mplumer@users.noreply.github.com>
2021-03-16 01:39:09 -0400

    clean fs

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,34 +14,14 @@ exports.onCreateNode = ({node , actions}) => {
             name: 'slug',
             value: slugFromTitle,
         });
-
-        if (Object.prototype.hasOwnProperty.call(node.frontmatter, "author")) {
-            createNodeField({
-              node,
-              name: "authorId",
-              value: slugify(node.frontmatter.author)
-            });
-        }
     }
-    
-    if(node.internal.type === 'AuthorsJson'){
-        createNodeField({
-            node,
-            name: "authorId",
-            value: slugify(node.name)
-        });
-    }
-
 }
-
+        
 exports.createPages = ({actions, graphql}) => {
     const { createPage } = actions;
     const templates =  {
         projectDetails: path.resolve('src/template/project-details.js'),
-        blogDetails: path.resolve('src/template/blog-details.js'),
-        categoryPost: path.resolve('src/template/category-post.js'),
-        tagPost: path.resolve('src/template/tag-template.js'),
-        authorPage: path.resolve('src/template/archive.js'),
+       
     }
 
     return graphql(`
@@ -54,31 +34,12 @@ exports.createPages = ({actions, graphql}) => {
                 }
             }
 
-
-            allMarkdownRemark {
-                edges {
-                    node {
-                        fields {
-                            slug
-                            authorId
-                        }
-                        frontmatter {
-                            author {
-                                name
-                            }
-                            tags
-                            category
-                        }
-                    }
-                }
-            }
-            
-
         }
+           
     `).then( res => {
         if (res.errors) return Promise.reject(res.errors)
         const project = res.data.allProjectJson.edges
-        const posts = res.data.allMarkdownRemark.edges
+        // const posts = res.data.allMarkdownRemark.edges
 
          // Create Project Page
          project.forEach(({ node }) => {
@@ -93,88 +54,86 @@ exports.createPages = ({actions, graphql}) => {
         })
 
         // Create Single Blog Page 
-        posts.forEach(({ node }) => {
-            createPage({
-                path: `${slugify(node.fields.slug)}`,
-                component: templates.blogDetails,
-                context: {
-                    slug: node.fields.slug
-                }
-            })
-        })
+        // posts.forEach(({ node }) => {
+        //     createPage({
+        //         path: `${slugify(node.fields.slug)}`,
+        //         component: templates.blogDetails,
+        //         context: {
+        //             slug: node.fields.slug
+        //         }
+        //     })
+        // })
 
-        // Create Single Blog Page 
-
-        // Start Category Area 
+       
 
         // For get All Categiry Pages 
-        let categories = []
-        _.each(posts , edge => {
-            if (_.get(edge , 'node.frontmatter.category')) {
-                categories = categories.concat(edge.node.frontmatter.category)
-            }
-        })
+        // let categories = []
+        // _.each(posts , edge => {
+        //     if (_.get(edge , 'node.frontmatter.category')) {
+        //         categories = categories.concat(edge.node.frontmatter.category)
+        //     }
+        // })
 
-        // [design , code]
-        let categoryPostCounts = {}
-        categories.forEach( category => {
-            categoryPostCounts[category] = (categoryPostCounts[category] || 0) + 1
-        })
-        categories = _.uniq(categories)
+        // // [design , code]
+        // let categoryPostCounts = {}
+        // categories.forEach( category => {
+        //     categoryPostCounts[category] = (categoryPostCounts[category] || 0) + 1
+        // })
+        // categories = _.uniq(categories)
 
        
         // Create Tag Posts Pages for indivedual Tag page
-        categories.forEach(category => {
-            createPage({
-                path: `/category/${slugify(category)}`,
-                component: templates.categoryPost,
-                context: {
-                    category 
-                }
-            })
-        })
+        // categories.forEach(category => {
+        //     createPage({
+        //         path: `/category/${slugify(category)}`,
+        //         component: templates.categoryPost,
+        //         context: {
+        //             category 
+        //         }
+        //     })
+        // })
         // End Category Area
 
 
 
         // Start Tags Pages 
-        let tags = []
-        _.each(posts , edge => {
-            if (_.get(edge , 'node.frontmatter.tags')) {
-                tags = tags.concat(edge.node.frontmatter.tags)
-            }
-        })
-        // Create Tag Posts Pages for indivedual Tag page
-        tags.forEach(tag => {
-            createPage({
-                path: `/tag/${slugify(tag)}`,
-                component: templates.tagPost,
-                context: {
-                    tag
-                }
-            })
-        })
+        // let tags = []
+        // _.each(posts , edge => {
+        //     if (_.get(edge , 'node.frontmatter.tags')) {
+        //         tags = tags.concat(edge.node.frontmatter.tags)
+        //     }
+        // })
+        // // Create Tag Posts Pages for indivedual Tag page
+        // tags.forEach(tag => {
+        //     createPage({
+        //         path: `/tag/${slugify(tag)}`,
+        //         component: templates.tagPost,
+        //         context: {
+        //             tag
+        //         }
+        //     })
+        // })
         // End Category Area
 
 
 
         // Start Create Authors Page 
-        let authors = []
-        _.each(posts, edge => {
-            if(_.get(edge, 'node.fields.authorId')){
-                authors = authors.concat(edge.node.fields.authorId)
-            }
-        })
-        authors = _.uniq(authors)
-        authors.forEach(author => {
-            createPage({
-                path: `/author/${slugify(author)}`,
-                component: templates.authorPage,
-                context: {
-                    author
-                }
-            })
-        })
+        // let authors = []
+        // _.each(posts, edge => {
+        //     if(_.get(edge, 'node.fields.authorId')){
+        //         authors = authors.concat(edge.node.fields.authorId)
+        //     }
+        // })
+        // authors = _.uniq(authors)
+        // authors.forEach(author => {
+        //     createPage({
+        //         path: `/author/${slugify(author)}`,
+        //         component: templates.authorPage,
+        //         context: {
+        //             author
+        //         }
+        //     })
+        // })
         // End Create Authors Page
 
 
